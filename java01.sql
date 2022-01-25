c717ba06dc2438ccbd8ee2058e096dcffe93769c
tobe1205 <tobe1205@hanmail.net>
2022-01-25 12:55:56 +0900

    [2022.01.25] 오라클 세이브

--- java01.sql
+++ java01.sql
@@ -128,6 +128,20 @@ WHERE board_no = 6;
 COMMIT;
 
 
+-- 회원 관리 테이블
+CREATE TABLE member (
+    account VARCHAR2(50),
+    password VARCHAR2(150) NOT NULL,
+    name VARCHAR2(50) NOT NULL,
+    email VARCHAR2(100) NOT NULL UNIQUE,
+    auth VARCHAR2(20) DEFAULT 'COMMON',
+    reg_date DATE DEFAULT SYSDATE,
+    CONSTRAINT pk_member PRIMARY KEY (account)
+);
+
+SELECT * FROM member;
+
+SELECT COUNT(*) FROM board;
 
 
 
