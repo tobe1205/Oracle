7643077fe27678acfba6f8aed61ae6ab292255bc
tobe1205@hanmail.net <tobe1205@hanmail.net>
2022-01-28 23:06:51 +0900

    [22.01.28] 오라클 세이브

--- java01_2.sql
+++ java01_2.sql
@@ -11,5 +11,19 @@ SET auth = 'ADMIN'
 WHERE account='admin';
 
 COMMIT;
-
 SELECT *FROM board;
+SELECT *FROM member;
+
+
+SELECT*FROM board2;
+
+ALTER TABLE board2 ADD account VARCHAR(100);
+COMMIT;
+
+SELECT *FROM member2;
+UPDATE member2
+SET auth = 'ADMIN'
+WHERE account='admin';
+
+COMMIT;
+
